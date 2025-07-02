ecf50b700a4e99f97b8bb079dad94a89d62ad029
SUPRIYAKUKUNURU <supriyakukunuru2001@gmail.com>
2025-07-02 11:01:00 +0530

    first  commit

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,6 +6,7 @@ export const Sidebar = () => {
   const onDragStart = (event, nodeType) => {
     event.dataTransfer.setData("application/reactflow", nodeType);
     event.dataTransfer.effectAllowed = "move";
+    
   };
 
   return (
